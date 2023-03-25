bc3871edc91e88c4f79fd4f1fce9b3adc17637d9
lelymaria <lelymariakova@gmail.com>
2023-03-25 20:20:44 +0700

    Update 3

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,19 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Gate::define("operator", function($user){
+            return $user->role->name === "Operator";
+        });
+        Gate::define("ketua P4MP", function($user){
+            return $user->role->name === "Ketua P4MP";
+        });
+        Gate::define("tim Auditor", function($user){
+            return $user->role->name === "Tim Auditor";
+        });
+        Gate::define("auditee", function($user){
+            return $user->role->name === "Auditee";
+        });
     }
 }
