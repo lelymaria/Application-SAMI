414cda1048e55a60e4c46c3be077f75e6a63a2cf
lelymaria <lelymariakova@gmail.com>
2023-07-30 20:04:24 +0700

    wrong name title page

--- resources/views/manage_akun/auditee/akun_auditee.blade.php
+++ resources/views/manage_akun/auditee/akun_auditee.blade.php
@@ -88,7 +88,7 @@
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title">Tambah Akun Jurusan</h5>
+                                <h5 class="modal-title">Tambah Akun Auditee</h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal">
                                 </button>
                             </div>
