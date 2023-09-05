28665b0c79d2669a973524c607950d13b3cd545b
lelymaria <lelymariakova@gmail.com>
2023-09-05 07:53:32 +0700

    new seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,26 +29,15 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
-        // $users = [
-        //     ["nip" => 2003071, "level_id" => Level::where("name", "Operator")->first()->id, "password" => bcrypt("password")],
-        //     ["nip" => 2003073, "level_id" => Level::where("name", "Ketua P4MP")->first()->id, "password" => bcrypt("password")],
-        //     ["nip" => 2003075, "level_id" => Level::where("name", "Lead Auditor")->first()->id, "password" => bcrypt("password")],
-        //     ["nip" => 2003076, "level_id" => Level::where("name", "Anggota Auditor")->first()->id, "password" => bcrypt("password")],
-        //     ["nip" => 2003077, "level_id" => Level::where("name", "Auditee")->first()->id, "password" => bcrypt("password")],
-        //     ["nip" => 2003079, "level_id" => Level::where("name", "Jurusan")->first()->id, "password" => bcrypt("password")]
-        // ];
-
-        // foreach ($users as $user) {
-        //     User::create($user);
-        // }
         $akunOperator = User::create([
-            "nip" => 2003071,
+            "nip" => 199602092019031011,
             "level_id" => Level::where("name", "Operator")->first()->id,
             "password" => bcrypt("password"),
-            "foto_profile" => 'images/profile/profile.png']);
+            "foto_profile" => 'images/profile/profile.png'
+        ]);
         $akunOperator->akunOperator()->create([
             "email" => "operator@gmail.com",
-            "nama" => "Lely Maria Kova"
+            "nama" => "Tantowi Yahya Yogas Tamara, A.Md.Kom."
         ]);
     }
 }
